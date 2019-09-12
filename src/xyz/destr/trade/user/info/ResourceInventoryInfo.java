b296fba467a331ca111e247fc0e6dfc82bb1958d
Destructor <destrovi4@gmail.com>
2019-09-12 16:00:55 +0400

    Добавлены гетеры

--- src/xyz/destr/trade/user/info/ResourceInventoryInfo.java
+++ src/xyz/destr/trade/user/info/ResourceInventoryInfo.java
@@ -4,5 +4,9 @@ import xyz.destr.trade.resource.ResourceType;
 
 public class ResourceInventoryInfo implements UserInfo {
 	private static final long serialVersionUID = -5946594202470880660L;
-	public final long[] resources = new long[ResourceType.values.length];
+	public final long[] amount = new long[ResourceType.values.length];
+	
+	public long getAmount(ResourceType resourceType) {
+		return amount[resourceType.ordinal()];
+	}
 }
--- src/xyz/destr/trade/user/info/ResourceMarketInfo.java
+++ src/xyz/destr/trade/user/info/ResourceMarketInfo.java
@@ -4,7 +4,19 @@ import xyz.destr.trade.resource.ResourceType;
 
 public class ResourceMarketInfo implements UserInfo {
 	private static final long serialVersionUID = -4491611244274329319L;
-	public final long[] resources = new long[ResourceType.values.length];
+	public final long[] amount = new long[ResourceType.values.length];
 	public final long[] purchasePrice = new long[ResourceType.values.length];
 	public final long[] sellingPrice = new long[ResourceType.values.length];
+	
+	public long getAmount(ResourceType resourceType) {
+		return amount[resourceType.ordinal()];
+	}
+	
+	public long getPurchasePrice(ResourceType resourceType) {
+		return purchasePrice[resourceType.ordinal()];
+	}
+	
+	public long getSellingPrice(ResourceType resourceType) {
+		return sellingPrice[resourceType.ordinal()];
+	}
 }
